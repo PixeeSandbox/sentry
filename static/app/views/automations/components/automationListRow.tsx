14970f9f63d93ce69cea64413b3b9e0ec3455832
Nate Moore <nate.moore@sentry.io>
2025-04-18 16:06:34 -0500

    giant demo mess

--- static/app/views/automations/components/automationListRow.tsx
+++ static/app/views/automations/components/automationListRow.tsx
@@ -5,6 +5,7 @@ import {Flex} from 'sentry/components/container/flex';
 import {Checkbox} from 'sentry/components/core/checkbox';
 import InteractionStateLayer from 'sentry/components/interactionStateLayer';
 import Link from 'sentry/components/links/link';
+import {ProjectList} from 'sentry/components/projectList';
 import {ActionCell} from 'sentry/components/workflowEngine/gridCell/actionCell';
 import {ConnectionCell} from 'sentry/components/workflowEngine/gridCell/connectionCell';
 import {TimeAgoCell} from 'sentry/components/workflowEngine/gridCell/timeAgoCell';
@@ -18,7 +19,7 @@ type AutomationListRowProps = {
   automation: Automation;
   handleSelect: (id: string, checked: boolean) => void;
   selected: boolean;
-};
+}
 
 export function AutomationListRow({
   automation,
@@ -49,6 +50,9 @@ export function AutomationListRow({
       </CellWrapper>
       <CellWrapper className="action">
         <ActionCell actions={actions} disabled={disabled} />
+      </CellWrapper>
+      <CellWrapper className="projects">
+        <ProjectList projectSlugs={[]} />
       </CellWrapper>
       <CellWrapper className="connected-monitors">
         <ConnectionCell ids={detectorIds} type="detector" disabled={disabled} />
